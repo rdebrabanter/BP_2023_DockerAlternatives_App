38efb00965327dc3d048030767bca398159d706d
rdebrabanter <robin.debrabanter@student.hogent.be>
2023-04-23 14:07:20 +0200

    Added domain + dto

---
+++ API/src/main/java/com/dockerbt/api/domain/Tool.java
@@ -0,0 +1,12 @@
+package com.dockerbt.api.domain;
+
+import java.time.LocalDateTime;
+
+public class Tool
+{
+    private Long id;
+    private String toolNumber;
+    private String department;
+    private String serialNumber;
+    private LocalDateTime dateInUse;
+}
