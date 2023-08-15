b3fd4750e7f13229a7200aef15c5dbbd34e6d1d9
De Brabanter, Robin <rdebrabanter@volvocars.com>
2023-08-15 10:50:04 +0200

    Updated controller endpoint

--- API/src/main/java/com/dockerbt/api/controllers/ToolController.java
+++ API/src/main/java/com/dockerbt/api/controllers/ToolController.java
@@ -16,7 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/toolshop/tools")
+@RequestMapping("/api/dockerbt/tools")
 @OpenAPIDefinition(tags ={@Tag(name="Tools", description = "API to manage tools.")})
 @RestController
 @Slf4j
